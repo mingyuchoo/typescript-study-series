927435e25129585e1f25ce2ccc36909b34acc6b7
mingyuchoo <mingyuchoo@gmail.com>
2020-06-23 07:19:28 +0900

    modified

--- src/file.spec.ts
+++ src/file.spec.ts
@@ -5,8 +5,17 @@ import * as file from './file';
 
 describe('file', () => {
   describe('fileRead()', () => {
+    const data = [
+      {
+        Message: 'Hello, World!',
+        category: 'TypeSciprt',
+        group: 'Programming',
+      },
+      { Message: 'Lean Start up', category: 'Agile', group: 'Methodology' },
+    ];
+
     it('should return "Hello, World!"', () => {
-      file.fileRead().should.equal('Hello, World!');
+      file.fileRead().should.eql(data);
     });
   });
 });
--- src/file.ts
+++ src/file.ts
@@ -4,7 +4,16 @@ import path from 'path';
 export function fileRead() {
   try {
     const data = fs.readFileSync(path.resolve(__dirname, './data.txt'), 'utf8');
-    return data;
+    const json = data.split('\n').map((record) => {
+      const item = record.split('%|;');
+      return {
+        Message: item[0],
+        category: item[1],
+        group: item[2],
+      };
+    });
+    console.log('>>> ', json);
+    return json;
   } catch (error) {
     return error;
   }
